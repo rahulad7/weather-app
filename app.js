d4a5305f32ea2bdd8b3b1eb339ffc885261a3610
Rahul A D <rahuldharwdakar5@gmail.com>
2023-08-11 12:17:07 +0530

    changed to declartive

--- app.js
+++ app.js
@@ -7,64 +7,13 @@ const fetchWeatherDataByCountry = async (city) => {
   return data;
 };
 
-<<<<<<< HEAD
-let target = 'bangalore'
-const fetchResults = async (cities) => {
-    const api_key = "API KEY"
-    let url = `https://api.weatherapi.com/v1/forecast.json?key=${api_key}&q=${cities}&days=7`
-    const res = await fetch(url)
-    const data = await res.json()
-    console.log(data);
-    document.querySelector(".temp").innerHTML = data.current.temp_c + "°c";
-    document.querySelector(".city").innerHTML = data.location.name;
+let cities = []; // Array to store the city data
 
-    const localTime = new Date( data.location.localtime).toLocaleDateString("en-US", {
-        weekday: "long",
-        year: "numeric",
-        month: "short",
-        day: "numeric",
-      });
-    document.querySelector(".time").innerHTML = localTime;
-
-    const time = new Date( data.location.localtime).toLocaleTimeString("en-US", {
-        hour: "numeric",
-        minute: "numeric",
-        hour12: true,
-      });
-    document.querySelector(".hi-low").innerHTML = time;
-
-    document.querySelector(".image").innerHTML = `<img src= "https:${data.current.condition.icon}"/>`;
-    document.querySelector(".weather").innerHTML = data.current.condition.text;
-    const firstday = new Date(data.forecast.forecastday[0].date).toLocaleString('en-us', { weekday: 'short' });
-    const secondday = new Date(data.forecast.forecastday[1].date).toLocaleString('en-us', { weekday: 'short' });
-    const thirdday = new Date(data.forecast.forecastday[2].date).toLocaleString('en-us', { weekday: 'short' });
-    document.querySelector("#date").innerHTML = firstday;
-    document.querySelector("#date1").innerHTML = secondday;
-    document.querySelector("#date2").innerHTML = thirdday;
-
-    document.querySelector(".day-temp0").innerHTML = data.forecast.forecastday[0].day.avgtemp_c + "°c";
-    document.querySelector(".day-temp1").innerHTML = data.forecast.forecastday[1].day.avgtemp_c + "°c";
-    document.querySelector(".day-temp2").innerHTML = data.forecast.forecastday[2].day.avgtemp_c + "°c";
-    document.querySelector(".day-image0").innerHTML = `<img src = "https:${data.forecast.forecastday[0].day.condition.icon}"/>`;
-    document.querySelector(".day-image1").innerHTML = `<img src = "https:${data.forecast.forecastday[1].day.condition.icon}"/>`;
-    document.querySelector(".day-image2").innerHTML = `<img src = "https:${data.forecast.forecastday[2].day.condition.icon}"/>`;
-
-}
-
-const searchInput = document.getElementById("searchInput")
-const cityList = document.getElementById("cityList")
-const searchform = document.querySelector(".searchform")
-const result = document.querySelector(".result")
-const searchbtn = document.querySelector(".searchbtn")
-
-let cities = [];  // Array to store the city data
-=======
 const searchInput = document.getElementById("searchInput");
 const cityList = document.getElementById("cityList");
 const searchform = document.querySelector(".searchform");
 const result = document.querySelector(".result");
 const searchbtn = document.querySelector(".searchbtn");
->>>>>>> c2a95df (changed to declarative)
 
 // Function to fetch the list of cities from the API
 async function fetchCities() {
